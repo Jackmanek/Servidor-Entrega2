03e024d243bd86c9c1f7123ddd10e65c27762dfb
Jasio <age002@gmail.com>
2022-10-14 23:06:19 +0200

    Ultmimo cambio 23:06 del 14.10 no puedo hacer mas mejoras

--- main.php
+++ main.php
@@ -7,6 +7,33 @@
     <title>Peliculas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<style>
+     body{
+        display: flex;
+        justify-content: center;
+    }
+    .contenedor{
+        border:2px solid red;
+        width:300px;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+    .verde{
+        border: 1px solid green;
+        color:white;
+        background-color: green;
+        text-align:center;
+        text-transform: uppercase;
+    }
+    .rojo{
+        border: 1px solid red;
+        color:white;
+        background-color: red;
+        text-align:center;
+        text-transform: uppercase;
+    }
+</style>
 </head>
 <body>
     <pre>
@@ -28,25 +55,27 @@
             $peliculas = Array();
 
             //si esta definida la variable listaAgenda (si existe el textArea con la lista)
-            if (isset($_POST['listaAgenda'])) {
+            if (isset($_POST['listaPeliculas'])) {
                 //separamos y almacenamos dentro de un array los items de la lista textArea
-                $peliculas = explode("\n", trim($_POST['listaAgenda']));
+                $peliculas = explode("\n", trim($_POST['listaPeliculas']));
             }
             //valido que no esten vacios los campos
             if (!empty($titulo_0) && !empty($isan_0)) {
-                //valido que el nombre no exista
-                if (!existeNombre($titulo_0, $peliculas)) {
+                //valido que el nombre no exista y que el ISAN tiene 8 digitos
+                if (!existeNombre($titulo_0, $peliculas) && ((strlen($isan_0)>=8) && (strlen($isan_0)<=8))) {
                     //agrego al array los nuevos datos
                     $peliculas[] = trim($titulo_0) . '-' . trim($isan_0). '-' . trim($anio_0). '-' . trim($puntos_0);
-                    echo "registro hecho";
+                    echo "<p class='verde'>registro hecho</p>";
                 } else {
-                    echo "El titulo ya esta registrado";
+                    echo "<p class='rojo'>El titulo ya esta registrado ó El ISAN no tiene 8 digitos</p>";
                     
                 }
             } else {
-                echo "Campos vacios, rellenalos porfavor";
+                echo "<p class='rojo'>Campos vacios, rellenalos porfavor</p>";
             }
-
+            if (!empty($titulo_0) && empty($isan_0)){
+                echo existeNombres($titulo_0, $peliculas);
+            }
 
             dibujaenOculto($peliculas);
             pintaTabla($peliculas);
@@ -56,18 +85,36 @@
             $existe = false;
             //recorreo el array
             foreach ($peliculas as $item) {
-                //separo el nombre del numero
+                //separo el titulo
                 $res = explode("-", trim($item));
-                //pregunto por el nombre
+                //pregunto por el titulo
                 if ($res[0] == $titulo_0) {
                     $existe = true;
                 }
             }
             return $existe;
         }
+        function existeNombres($titulo_0, $peliculas) {
+            
+            //recorreo el array
+            foreach ($peliculas as $item) {
+                
+                $res = explode("-", trim($item));
+                
+                // if ($res[0] == $titulo_0) {
+                //     $existe = true;
+                // }
+                if (strncmp($res[0], $titulo_0, 5) === 0)
+                    echo "Los strings coinciden";
+                    print_r($peliculas);
+                
+            }
+            
+        }
+
 
         function dibujaenOculto($peliculas) {
-            $txtArea = "<textarea style='display: none' name='listaAgenda' form='main'>";
+            $txtArea = "<textarea style='display: none' name='listaPeliculas' form='main'>";
             foreach ($peliculas as $item) {
                 $txtArea .= trim($item) . "\n";
             }
@@ -76,7 +123,8 @@
         }
 
         function pintaTabla($peliculas) {
-            $table = "<table border='1' >";
+            echo "<div class='contenedor'>";
+            $table = "<table>";
             $table .= "<tr>";
             $table .= "<td> Titulo </td>";
             $table .= "<td> ISAN </td>";
@@ -95,6 +143,7 @@
             }
             $table .= "</table>";
             echo $table;
+            echo "</div>";
         }
         print_r($_POST);
         ?>
@@ -116,7 +165,6 @@
             <!-- <input type="hidden" name="list_Pelis" id="" value="<?php echo $list_Pelis ?>"> -->
             <input type="submit" name="guardar" value="Guardar">
         </form>
-       
-    </pre>
+        <pre>
 </body>
 </html>
